d5cb1e52f65d566b165e812c94fc5c63856537ad
lev <lleevv2020@gmail.com>
2023-12-01 19:38:48 +0400

    add

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,14 @@
 import 'react-toastify/dist/ReactToastify.css'
 
+import Header from '../Header'
 import './App.css'
 
 function App() {
-  return <div className="app">jjjjj</div>
+  return (
+    <div className="app">
+      <Header className="app__header" />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+export default function Header() {
+  return (
+    <header className={'header'}>
+      <div className="header__logo">Realworld Blog</div>
+      <div className="header__controls">Create article</div>
+    </header>
+  )
+}

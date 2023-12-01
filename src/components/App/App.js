df68bfbf4a3af488b95e5d33a2e7f8f43749d837
lev <lleevv2020@gmail.com>
2023-12-01 18:33:13 +0400

    add

---
+++ src/components/App/App.js
@@ -0,0 +1,9 @@
+import 'react-toastify/dist/ReactToastify.css'
+
+import './App.css'
+
+function App() {
+  return <div className="app">jjjjj</div>
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('test')
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import App from './components/App/'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<App />)

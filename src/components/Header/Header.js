9be9c8e7a9fff83cd7614bc2bb92f802205f2500
lev <lleevv2020@gmail.com>
2023-12-04 16:15:47 +0400

    add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,29 @@
+import { Link } from 'react-router-dom'
 import './style.css'
+import { Button } from 'antd'
+
+import { AppRoute } from '../../constants'
 
 export default function Header() {
   return (
     <header className={'header'}>
-      <div className="header__logo">Realworld Blog</div>
-      <div className="header__controls">Create article</div>
+      <div className="header__logo">
+        <Link className="header__logo-link" to={AppRoute.Root}>
+          Realworld Blog
+        </Link>
+      </div>
+      <div className="header__controls">
+        <div className="header__unauth-user">
+          <Link to={AppRoute.Login}>
+            <Button className="header__sign-in" type="text">
+              Sign In
+            </Button>
+          </Link>
+          <Link to={AppRoute.Registration}>
+            <Button className="header__sign-up">Sign Up</Button>
+          </Link>
+        </div>
+      </div>
     </header>
   )
 }
---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const AppRoute = {
+  Root: '/',
+  Articles: '/articles',
+  Article: '/articles/:slug',
+  Login: '/sign-in',
+  Registration: '/sign-up',
+  Profile: '/profile',
+  NewArticle: '/new-article',
+  EditArticle: '/articles/:slug/edit',
+  NotFound: '*',
+}

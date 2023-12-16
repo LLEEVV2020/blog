dd0876e046902f28c34f34e80f93cfaaafb6ce26
lev <lleevv2020@gmail.com>
2023-12-16 17:14:11 +0400

    add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,19 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import './style.css'
 import { Button } from 'antd'
+import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 import { AppRoute } from '../../constants'
+import { removeUserAction } from '../../state/userReducer'
+import defaultAvatar from './images/defaultAvatar.png'
 
 export default function Header(props) {
   const { className } = props
+  const user = useSelector((state) => state.userInfo.user)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
   return (
     <header className={`${className} header`}>
       <div className="header__logo">
@@ -14,16 +22,37 @@ export default function Header(props) {
         </Link>
       </div>
       <div className="header__controls">
-        <div className="header__unauth-user">
-          <Link to={AppRoute.Login}>
-            <Button className="header__sign-in" type="text">
-              Sign In
+        {user ? (
+          <div className="header__auth-user">
+            <Link to={AppRoute.NewArticle}>
+              <Button className="header__create-article">Create article</Button>
+            </Link>
+            <div className="header__user-profile">
+              <Link to={AppRoute.Profile} className="header__user-name">
+                {user.username}
+              </Link>
+              <Link to={AppRoute.Profile} className="header__user-avatar">
+                <img src={user.image || defaultAvatar} width="46" height="46" alt="user avatar " />
+              </Link>
+            </div>
+            <Button className="header__logout" onClick={() => dispatch(removeUserAction())}>
+              Log Out
             </Button>
-          </Link>
-          <Link to={AppRoute.Registration}>
-            <Button className="header__sign-up">Sign Up</Button>
-          </Link>
-        </div>
+          </div>
+        ) : (
+          <div className="header__unauth-user">
+            <Link to={AppRoute.Login}>
+              <Button className="header__sign-in" type="text" onClick={() => navigate(AppRoute.Login)}>
+                Sign In
+              </Button>
+            </Link>
+            <Link to={AppRoute.Registration}>
+              <Button className="header__sign-up" onClick={() => navigate(AppRoute.Registration)}>
+                Sign Up
+              </Button>
+            </Link>
+          </div>
+        )}
       </div>
     </header>
   )
